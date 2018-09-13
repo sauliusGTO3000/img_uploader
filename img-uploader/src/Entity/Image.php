fcebcf51b9b3823edef2c08577d11b8e5f5b9776
sauliusgto3000 <sauliusgto3000@gmail.com>
2018-09-13 04:11:03 +0300

    file upload validation added

--- img-uploader/src/Entity/Image.php
+++ img-uploader/src/Entity/Image.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ImageRepository")
@@ -23,6 +24,11 @@ class Image
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\File(
+     *     maxSize = "20M",
+     *     mimeTypes = {"image/jpeg", "image/png", "image/gif"},
+     *     mimeTypesMessage = "Please upload a valid image, allowed types: .jpg, .png, .gif"
+     *     )
      */
     private $filename;
 
--- img-uploader/src/Service/FileUploader.php
+++ img-uploader/src/Service/FileUploader.php
@@ -22,7 +22,7 @@ class FileUploader
 
     public function upload(UploadedFile $file)
     {
-        $fileName = md5(uniqid()).'.'.$file->guessExtension();
+        $fileName = md5(uniqid()).'.'.$file->getClientOriginalExtension();
 
         $file->move($this->getTargetDirectory(), $fileName);
 
